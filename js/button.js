55a8afdfa34e913063bbcff91aa4954d832e2d4b
jasonouyang800 <61800616+jasonouyang800@users.noreply.github.com>
2020-03-18 13:32:02 -0700

    changed button

---
+++ js/button.js
@@ -0,0 +1,25 @@
+
+$( document ).ready(function(){
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 100, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            console.log('test')
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }
+})
